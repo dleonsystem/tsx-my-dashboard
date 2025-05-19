24f1c7bbf1ae2935c8ba11aba928250f5835674c
David León Gómez <dleon55@hotmail.com>
2025-05-19 01:26:14 -0600

    mejora de estándar en hoja de estilos implementando clases

--- src/pages/PaisesPage.tsx
+++ src/pages/PaisesPage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import UserList from "../components/CountriesList";
 import { useTranslation } from "react-i18next";
-
+import styles from "../styles/PaisesPage.module.css";
 
 const PaisesPage: React.FC = () => {
     // Aquí puedes usar el hook useTranslation para traducir textos
   const { t } = useTranslation();
   return (
-    <div style={{ padding: "20px" }}>
+    <div className={styles.container}>
       <h1>🌍 {t("countries.titlePage")}</h1>
       <UserList />
     </div>
